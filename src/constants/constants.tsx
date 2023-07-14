10aa34c96be79b8cd38817ca62327aeacfc73b4c
Alwaz <alwazkazi3@gmail.com>
2023-07-15 01:12:52 +0500

    removed image from footer

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -158,6 +158,32 @@ export const footerLinks = [{
   ],
 
 },
+{
+  title: "Lorem Ipsum",
+  links: [
+    {
+      name: "Content",
+      link: "https://www.hoobank.com/content/",
+    },
+    {
+      name: "How it Works",
+      link: "https://www.hoobank.com/how-it-works/",
+    },
+    {
+      name: "Create",
+      link: "https://www.hoobank.com/create/",
+    },
+    {
+      name: "Explore",
+      link: "https://www.hoobank.com/explore/",
+    },
+    {
+      name: "Terms & Services",
+      link: "https://www.hoobank.com/terms-and-services/",
+    },
+  ],
+
+},
 ]
 
 
