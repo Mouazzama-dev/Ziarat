afbe395764f27c82b5a41378c60ddd3c2c4ae203
Alwaz <alwazkazi3@gmail.com>
2023-07-15 21:32:18 +0500

    feat: Added loader on form submit

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Card, CardFooter, Checkbox, Dialog, DialogBody, DialogHeader, Input, Textarea, Typography, } from '@material-tailwind/react'
+import { Button, Card, CardFooter, Checkbox, Dialog, DialogBody, DialogHeader, Input, Spinner, Textarea, Typography, } from '@material-tailwind/react'
 import { ModalProps } from '../../constants/constants'
 import { addDoc, collection } from 'firebase/firestore'
 import { db } from '../../config/firebase'
@@ -9,18 +9,21 @@ import { db } from '../../config/firebase'
 const ContactModal: React.FC<ModalProps> = ({ isOpen, handleOpen, heading, showCheckbox, buttonText }) => {
     const [email, setEmail] = useState<string>('')
     const [message, setMessage] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
+    const [formSubmit, setFormSubmit] = useState<boolean>(false);
 
     const userCollectionRef = collection(db, 'userMessages')
 
 
-    /* TODOs: 
-        - Modal button will have a loader.
-        - Loading stops Modal should display "Thankyou for Contact"
-     */
 
     const onSubmit = async () => {
         try {
-            await addDoc(userCollectionRef, { email, message })
+            setLoading(true)
+            const submitted = await addDoc(userCollectionRef, { email, message })
+            if (submitted) {
+                setLoading(false)
+                setFormSubmit(true)
+            }
 
         } catch (error) {
             console.error(error)
@@ -38,31 +41,33 @@ const ContactModal: React.FC<ModalProps> = ({ isOpen, handleOpen, heading, showC
         >
             <Card className="mx-auto max-w-[24rem] bg-black-gradient-2 ">
                 <DialogHeader className='flex justify-center items-center font-poppins text-white'  >{heading}</DialogHeader>
-                <DialogBody className="flex flex-col gap-4" >
-                    <Input label="Email" size="lg" color={'blue-gray'} className='text-white' required name='email' onChange={(e) => setEmail(e.target.value)} value={email} />
-
-                    <Textarea label="Message" color={'blue-gray'} className='text-white' name='message' onChange={(e) => setMessage(e.target.value)} value={message} />
-                    {showCheckbox && (
-
-                        <Checkbox
-                            color='amber'
-                            label={
-
-                                <Typography variant="small" className="font-normal text-dimWhite">
-                                    {`Keep me in the loop! occasional offers (optional)`}
-                                </Typography>
-
-                            }
-
-                        />
-                    )}
-
-                </DialogBody>
-                <CardFooter className="pt-0">
-                    <Button variant={'filled'} color={'white'} size="lg" onClick={onSubmit} fullWidth>
-                        {buttonText}
-                    </Button>
-                </CardFooter>
+                {!formSubmit ? (
+                    <DialogBody className="flex flex-col gap-4" >
+                        <Input label="Email" size="lg" color={'blue-gray'} className='text-white' required name='email' onChange={(e) => setEmail(e.target.value)} value={email} />
+                        <Textarea label="Message" color={'blue-gray'} className='text-white' name='message' onChange={(e) => setMessage(e.target.value)} value={message} />
+                        {showCheckbox && (
+                            <Checkbox
+                                color='amber'
+                                label={
+                                    <Typography variant="small" className="font-normal text-dimWhite">
+                                        {`Keep me in the loop! occasional offers (optional)`}
+                                    </Typography>
+                                }
+                            />
+                        )}
+                        <CardFooter className="p-0">
+                            <Button variant={'filled'} color={'white'} size="lg" fullWidth onClick={onSubmit} className='flex justify-center items-center' >
+                                {loading ? (<Spinner />) : (buttonText)}
+                            </Button>
+                        </CardFooter>
+                    </DialogBody>
+                ) : (
+                    <DialogBody className="flex flex-col justify-center items-center" >
+                        <Typography variant="h5" className=" text-white font-bold bg-borderDark p-2 text-center rounded-md border border-solid border-borderDark">
+                            {`Thank you! Your data has been submitted`}
+                        </Typography>
+                    </DialogBody>
+                )}
             </Card>
         </Dialog >
     )
