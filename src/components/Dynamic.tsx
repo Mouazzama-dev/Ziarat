d3a4e207d2e00d7a9f1cdb262ff2c60cff154c41
Alwaz <alwazkazi3@gmail.com>
2023-07-29 23:11:02 +0500

    improvement - added lazy loading

--- src/components/Dynamic.tsx
+++ src/components/Dynamic.tsx
@@ -16,13 +16,13 @@ const Dynamic: React.FC = () => {
 
             {/* ====> Image <==== */}
             <div className='relative'>
-                <img src={cube} alt="cube" />
+                <img src={cube} alt="cube" loading='lazy' />
 
                 <div className='absolute  hidden lg:block top-0 animate-bounce'>
-                    <img src={fullRing} alt='bouncing ring' />
+                    <img src={fullRing} alt='bouncing ring' loading='lazy' />
                 </div>
                 <div className='absolute  hidden lg:block -right-48 bottom-0 animate-bounce' >
-                    <img src={bowl} alt="bowl" />
+                    <img src={bowl} alt="bowl" loading='lazy' />
                 </div>
             </div>
         </section >
